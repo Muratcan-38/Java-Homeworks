c0c61c3ecf4c452639d419cbfaf4f21e1654cb5f
Muratcan Kaya <114094284+Muratcan-38@users.noreply.github.com>
2022-11-20 12:12:09 +0300

    Add files via upload

---
+++ isItVowels/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+       char harf='A';
+
+       switch (harf){
+           case 'a':
+           case 'e':
+           case 'i':
+           case 'o':
+           case 'u':
+           case 'A':
+           case 'E':
+           case 'I':
+           case 'O':
+           case 'U':
+               System.out.println("Vowels");
+               break;
+               default:
+                   System.out.println("Not Vowels");
+
+       }
+    }
+}
