cd75561cbb4992f25d8209e47f3f95c552295b1c
Muratcan Kaya <114094284+Muratcan-38@users.noreply.github.com>
2022-11-20 19:57:41 +0300

    Add files via upload

---
+++ classes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+        int[] numbers1 = new int[]{1,2,3};
+        int [] numbers2 = new int[]{4,5,6};
+        numbers2 = numbers1;
+        numbers1[0]=10;
+        System.out.println(numbers2[0]);
+    }
+}
+
